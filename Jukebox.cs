1070b88a4fc222059031049541bae000f17403c6
allenokech <38517628+allenokech@users.noreply.github.com>
2018-04-19 05:36:05 +0100

    Update Jukebox.cs

--- Jukebox.cs
+++ Jukebox.cs
@@ -19,6 +19,8 @@ namespace JukeBox
         {
             InitializeComponent();
         }
+    
+    // Show About Dialog //
     private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             (new AboutForm()).ShowDialog();
