28e180de897e3b2b7184298a5fe3a3a850cbbef4
allenokech <38517628+allenokech@users.noreply.github.com>
2018-04-19 05:45:25 +0100

    Update About.cs

--- About.cs
+++ About.cs
@@ -5,3 +5,15 @@ namespace JukeBox
 {
     public partial class AboutForm : Form
     {
+    
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Ok_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
