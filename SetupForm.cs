6d355ac70b0cf1153537cfc86707394d782aec33
allenokech <38517628+allenokech@users.noreply.github.com>
2018-04-19 05:47:28 +0100

    Update SetupForm.cs

--- SetupForm.cs
+++ SetupForm.cs
@@ -9,3 +9,21 @@ namespace JukeBox
 {
     public partial class SetupForm : Form
     {
+        private bool RSave = false;
+
+        private ListBox[] Setup_Media_Library;
+
+        private int Int_SetupNumberofGenre;
+
+        private int Int_ShowGenreNumber = 1;
+
+        private string Str_CopyTracksTo = string.Concat(Directory.GetCurrentDirectory(), "\\Tracks\\");
+
+        private string Setup_StrApplicationMediaPath = Directory.GetCurrentDirectory();
+
+        private string Str_CopyTracksFrom = "";
+
+        public SetupForm()
+        {
+            InitializeComponent();
+        }
