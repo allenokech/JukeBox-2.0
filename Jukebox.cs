b030ddb542dd8c1e2f96c50f6828b516c9fd951e
allenokech <38517628+allenokech@users.noreply.github.com>
2018-04-19 05:43:58 +0100

    Update Jukebox.cs

--- Jukebox.cs
+++ Jukebox.cs
@@ -27,3 +27,17 @@ namespace JukeBox
         {
             (new AboutForm()).ShowDialog();
         }
+        
+     private void hozScroll_Genre_ValueChanged(object sender, EventArgs e)
+        {
+            txt_Genre_Title.Text = Media_Library[hozScroll_Genre.Value - 1].Items[0].ToString();
+            AddToGList(hozScroll_Genre.Value - 1);
+        }
+
+        private bool Initailise()
+        {
+            hozScroll_Genre.Maximum = Int_NumberofGenre;
+            hozScroll_Genre.Value = hozScroll_Genre.Minimum;
+            AddToGList(hozScroll_Genre.Value - 1);
+            return true;
+        }
